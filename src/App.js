2ff3b71125c5496f226a579a1cde85e62130ad51
Vishal Chandna <chandna.vishal1234@gmail.com>
2018-08-27 19:27:50 +0530

    Necessary files added.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './Components/HomePage';
+import TodoList from './Components/TodoList';
 import './App.css';
+
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/todo-list" component={TodoList} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Components/TodoList.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listData: [],
+      listItem:'',
+    }
+  }
+  pushItem(e) {
+    e.preventDefault();
+    this.setState(() => {
+      let listData = this.state.listData
+      console.log(this.state)
+      return {
+        listData: this.state.listItem ? listData.concat([{value: this.state.listItem, id: Math.random(1)}]) : listData,
+        listItem:'',
+      }
+    })
+    return false;
+  }
+  removeItem(index) {
+    let listData = this.state.listData
+    listData.splice(index, 1)
+    this.setState(() => {
+      return {
+        listData: listData,
+      }
+    })
+  }
+  onInputChange(e) {
+    let listItem = e.target.value
+    this.setState(() => { return { listItem: listItem }})
+  }
+  render() { 
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="todo-list-container">
+          <h1>Welcome to Todo List</h1>
+          <div className="list-items">
+            <ul>
+              {this.state.listData.map((item, index) => {
+                return <li key={item.id}>
+                  <p>{index+1}. {item.value}</p>
+                  <button type="button" onClick={this.removeItem.bind(this, index)}>X</button>
+                </li>
+              })}
+            </ul>
+          </div>
+          <form onSubmit={this.pushItem.bind(this)}>
+            <input type="text" value={this.state.listItem} onChange={this.onInputChange.bind(this)} placeholder="Item Name"/>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
